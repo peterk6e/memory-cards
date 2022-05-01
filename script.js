21ef495a476a70d48d970a8749358985f29cce4c
pierrek6e <pierrecass75@gmail.com>
2022-05-01 11:28:33 +0700

    1st version

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
-const cardsEl = document.getElementById("cards");
 const cardContainerEl = document.getElementById("card-container");
 const newCardContainerEl = document.getElementById("new-card-container");
+const arrowL = document.getElementById("arrow-left");
+const arrowR = document.getElementById("arrow-right");
+const cardEl = document.getElementById("card");
+const cardsEl = document.getElementById("cards");
 const cards = [];
+var index = 0;
+cardEl.style.display = "none";
 
 function create() {
   const input_question = document.getElementById("question").value;
   const input_answer = document.getElementById("answer").value;
-  const card = { question: input_question, answer: input_answer };
+  const card = {
+    question: input_question,
+    answer: input_answer,
+  };
   cards.push(card); // will be saved in local storage
-  let { question, answer } = card;
-  const cardEl = document.createElement("div");
-
-  cardEl.className = "card";
-  cardEl.innerText = question;
-  cardEl.addEventListener("click", () => flipCard(answer, question, cardEl));
-
-  cardsEl.appendChild(cardEl);
 }
 
-function flipCard(answer, question, cardEl) {
+function displayCards() {
+  if (cardEl.style.display === "none") cardEl.style.display = "block"; // for 1st card
+  cardEl.innerText = cards[index].question;
+}
+
+function flipCard() {
+  let { question, answer } = cards[index];
   cardEl.innerText = cardEl.innerText === question ? answer : question;
 }
 
 function toggleContainer() {
   if (cardContainerEl.style.display == "none") {
-    cardContainerEl.style.display= "block";
+    cardContainerEl.style.display = "block";
     newCardContainerEl.style.display = "none";
   } else {
     cardContainerEl.style.display = "none";
@@ -32,9 +38,21 @@ function toggleContainer() {
   }
 }
 
+//Event listeners
 const formEl = document.getElementById("card-form");
 formEl.addEventListener("submit", (e) => {
   create();
+  displayCards();
   toggleContainer();
   e.preventDefault();
+});
+
+arrowL.addEventListener("click", () => {
+  index = index == 0 ? cards.length : --index;
+  displayCards();
+});
+
+arrowR.addEventListener("click", () => {
+  index = index == cards.length ? 0 : ++index;
+  displayCards();
 });
